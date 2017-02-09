67035e2f555b677df927f2cb9a648e452f3945e6
AlparAndras <alpar165@gmail.com>
2017-02-09 13:52:43 +0100

    index commited

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+  session_start();
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+  </head>
+  <body>
+    <form action="login.php" method="post">
+
+    </form>
+  </body>
+</html>
